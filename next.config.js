18a4cecc379a42e16f0051363556a45ca05d454c
Jeffy2600II <nontakorn2600@gmail.com>
2026-05-17 19:13:45 +0700

    update

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// Path:    next.config.js
+// Purpose: Next.js configuration for Opslert bot.
+// Used by: Next.js build process
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
